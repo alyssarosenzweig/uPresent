139d716e572704c207c7014171db9d5bd087b5e0
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-05-12 19:34:18 -0400

    Enhance script

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 document.addEventListener("keypress", slideSwitch);
 document.addEventListener("click", slideSwitch);
 
-function slideSwitch() {
+function slideSwitch(e) {
+	e.preventDefault();
 	window.scrollBy(0, window.innerHeight)
+}
+
+function load() {
+	window.scrollTo(0, 0);
 }

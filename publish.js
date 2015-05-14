fc30f622b9fe51e3dcc76292062c3c930a4555e7
Hardmath123 <hardmath123@gmail.com>
2015-05-13 18:01:15 -0700

    make up legit cli tool

--- publish.js
+++ publish.js
@@ -8,6 +8,33 @@ var grammar = require("./uPresent.ne.js");
 var nearley = require("nearley");
 var parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
 
-parser.feed(fs.readFileSync(process.argv[2]).toString());
+var opts = require("nomnom")
+    .script("up")
+    .option("input", {
+        "position": 0,
+        "required": true,
+        "help": "An input Markdown file"
+    })
+    .option("output", {
+        "abbr": "o",
+        "help": "An output html file (if not provided, write to stdout)"
+    })
+    .option('version', {
+        abbr: 'v',
+        flag: true,
+        help: "Print version and exit",
+        callback: function() {
+            return require('../package.json').version;
+        }
+    })
+    .parse();
 
-console.log(parser.results[0]);
+var input = fs.readFileSync(opts.input).toString();
+
+parser.feed(input);
+
+if (opts.output) {
+    fs.createWriteStream(opts.output).write(parser.results[0]);
+} else {
+    process.stdout.write(parser.results[0]);
+}

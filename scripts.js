92db6981b84874c6b6448523320598910917b610
Hardmath123 <hardmath123@gmail.com>
2015-05-20 16:33:26 -0700

    vanity is my favorite sin

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
-var currentSlide = 0;
+window.addEventListener("load", function(e) {
+    var currentSlide = 0;
 
-function slideSwitch(num) {
-	if(document.getElementById("slide"+(currentSlide + num))) {
-		currentSlide += num;
-		window.location.hash = "#slide"+currentSlide;
-	} // else slide doesn't exist and there's nothing to switch to
-}
+    function slideSwitch(num) {
+        if (document.getElementById("slide"+(currentSlide + num))) {
+            currentSlide += num;
+        } // else slide doesn't exist and there's nothing to switch to
+        window.location.hash = "#slide"+currentSlide;
+    }
+    document.addEventListener("keydown", function(e) {
+        //e.preventDefault();
+        if(e.keyCode === 37 || e.keyCode === 33 || e.keyCode === 38 || e.keyCode === 8) {
+            slideSwitch(-1);
+        } else {
+            slideSwitch(1);
+        }
+    });
 
-document.addEventListener("keydown", function(e) {
-		e.preventDefault();
+    document.addEventListener("click", function() {
+        slideSwitch(1)
+    });
 
-		if(e.keyCode == 37 || e.keyCode == 38) {
-			// up / left
-			slideSwitch(-1);
-		} else {
-			slideSwitch(1);
-		}
-	});
+    function wheel(e) {
+        e.preventDefault();
+    }
+    if (window.addEventListener) {
+      window.addEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = wheel;
 
-document.addEventListener("click", function() {
-		slideSwitch(1)
-	});
 
+    slideSwitch(0);
+});
 

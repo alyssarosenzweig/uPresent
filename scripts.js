46e04eba0133657926a60f459b5eb67c37eba753
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-05-30 09:04:23 -0400

    Fix more indentation issues

--- scripts.js
+++ scripts.js
@@ -5,65 +5,64 @@ window.addEventListener("load", function(e) {
 	var listIndex = -1;
 
 	function getBulletPointCount(slideNum) {
-	var slide = document.getElementById("slide" + slideNum);
+		var slide = document.getElementById("slide" + slideNum);
 
-	var count = 0;
-	
-	for(var i = 0; i < slide.children.length; ++i) {
-		if(slide.children[i].tagName == "UL") {
-			count += slide.children[i].children.length;
+		var count = 0;
+		
+		for(var i = 0; i < slide.children.length; ++i) {
+			if(slide.children[i].tagName == "UL") {
+				count += slide.children[i].children.length;
+			}
 		}
-	}
 
-	return count;
+		return count;
 	}
 
 	function slideSwitch(num) {
-	actualSlide = document.getElementById("slide"+currentSlide);
-	var slide = actualSlide;
+		actualSlide = document.getElementById("slide"+currentSlide);
+		var slide = actualSlide;
 
-	// only perform fancy bullet point transitions if requested by the presentation
-	
-	if(actualSlide.className.indexOf("transbullets") > -1) {
-		if(num > 0)
-			listIndex += num;
-
-		if(getBulletPointCount(currentSlide) > 0 && listIndex < getBulletPointCount(currentSlide)) {
-			for(var i = 0; i < slide.children.length; ++i) {
-				if(slide.children[i].tagName == "UL") {
-					var bullet = slide.children[i].children[listIndex];
-					if(bullet) bullet.style.visibility = bullet.style.visibility != 'visible' ? 'visible' : 'hidden';
-				}
-			}
-
-			if(num < 0)
+		// only perform fancy bullet point transitions if requested by the presentation
+		
+		if(actualSlide.className.indexOf("transbullets") > -1) {
+			if(num > 0)
 				listIndex += num;
 
-			if(listIndex > -2) return;
+			if(getBulletPointCount(currentSlide) > 0 && listIndex < getBulletPointCount(currentSlide)) {
+				for(var i = 0; i < slide.children.length; ++i) {
+					if(slide.children[i].tagName == "UL") {
+						var bullet = slide.children[i].children[listIndex];
+						if(bullet) bullet.style.visibility = bullet.style.visibility != 'visible' ? 'visible' : 'hidden';
+					}
+				}
 
+				if(num < 0)
+					listIndex += num;
+
+				if(listIndex > -2) return;
+			}
+
+			if(num > 0)
+				listIndex = -1;
+			else
+				listIndex = getBulletPointCount(currentSlide - 1) - 1;
 		}
 
-		if(num > 0)
-			listIndex = -1;
-		else
-			listIndex = getBulletPointCount(currentSlide - 1) - 1;
-	}
+		// only switch slides if there is a slide to switch to
 
-		if (document.getElementById("slide"+(currentSlide + num))) {
+		if (document.getElementById("slide"+(currentSlide + num)))
 			currentSlide += num;
-		} // else slide doesn't exist and there's nothing to switch to
+		
 		window.location.hash = "#slide"+currentSlide;
 	}
 
 	function makeBlankSlide(actuallyBlank) {
 		var slide = document.getElementById("slide"+(currentSlide));
 		
-		if(!blankSlide) {
-			if(actuallyBlank) {
-	   		slide.style.visibility = 'hidden';
+		if(!blankSlide && actuallyBlank) {
+			slide.style.visibility = 'hidden';
 			blankSlide = true;
-		}
-		} else {
+		} else if(blankSlide) {
 			slide.style.visibility = 'visible';
 			blankSlide = false;
 		}
@@ -73,39 +72,36 @@ window.addEventListener("load", function(e) {
 		e.preventDefault();
 		console.log(e.keyCode);
 	
-	if(e.keyCode == 17) return; // ctrl-key
+		if(e.keyCode == 17) return; // ctrl-key
 
-	if(e.keyCode === 37 || e.keyCode === 33 || e.keyCode === 38 || e.keyCode === 8) {
-		makeBlankSlide(false);
-			slideSwitch(-1);
-	} else if(e.keyCode == 190) { // blank slide
-		makeBlankSlide(true);
-	} else if(e.keyCode == 116 || e.keyCode == 27 || (e.keyCode == 70 && (e.ctrlKey || e.metaKey))) { // fullscreen --- start presentation button on my clicker
-	
-		var elem = document.documentElement;
+		if(e.keyCode === 37 || e.keyCode === 33 || e.keyCode === 38 || e.keyCode === 8) {
+			makeBlankSlide(false);
+				slideSwitch(-1);
+		} else if(e.keyCode == 190) { // blank slide
+			makeBlankSlide(true);
+		} else if(e.keyCode == 116 || e.keyCode == 27 || (e.keyCode == 70 && (e.ctrlKey || e.metaKey))) { // fullscreen --- start presentation button on my clicker
+			var elem = document.documentElement;
 
-		if(elem.requestFullScreen) elem.requestFullScreen();
-		else if(elem.webkitRequestFullScreen) {
-			elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-		} else if(elem.mozRequestFullScreen) {
-			elem.mozRequestFullScreen();
-		}
-	} else {
-		makeBlankSlide(false);
+			if(elem.requestFullScreen) elem.requestFullScreen();
+			else if(elem.webkitRequestFullScreen) {
+				elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+			} else if(elem.mozRequestFullScreen) {
+				elem.mozRequestFullScreen();
+			}
+		} else {
+			makeBlankSlide(false);
 			slideSwitch(1);
 		}
 	});
 
 	document.addEventListener("click", function(e) {
-	//e.preventDefault();
+		// switch differently based on which button is pressed
 
-	// switch differently based on which button is pressed
-
-	if(e.which == 1) {
-		slideSwitch(1);
-	} else {
-		slideSwitch(-1);
-	}
+		if(e.which == 1) {
+			slideSwitch(1);
+		} else {
+			slideSwitch(-1);
+		}
 	});
 
 	function wheel(e) {
@@ -116,8 +112,4 @@ window.addEventListener("load", function(e) {
 	  window.addEventListener('DOMMouseScroll', wheel, false);
 	}
 	window.onmousewheel = document.onmousewheel = wheel;
-
-
-	//slideSwitch(0);
 });
-

c43dd43bf4a906805f44d7204d5854d8ce5f0bb0
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-05-31 17:11:34 -0400

    Fix directory cluttering

--- browser/browser.js
+++ browser/browser.js
@@ -3,7 +3,7 @@
  * this bit is meant to be compiled to browserify
  */
 
-var backend = require("./publish_core.js");
+var backend = require("../publish_core.js");
 
 function publish_presentation(input_markdown) {
     return backend(input_markdown, false, false); // TODO: enable minification and other goodies with filesystem

8630812be5721be84568922a7e96a57041a747ca
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-05-31 17:20:16 -0400

    Render presentation to iframe

--- browser/browser.js
+++ browser/browser.js
@@ -5,8 +5,18 @@
 
 var backend = require("../publish_core.js");
 
+// calls the backend to perform the actual publishing
 function publish_presentation(input_markdown) {
     return backend(input_markdown, false, false); // TODO: enable minification and other goodies with filesystem
 }
 
-window.uPresent = publish_presentation; // this might work
+// renders the published presentation to an iframe for previewing the HTML
+function render_presentation(input) {
+    var iframe = document.getElementById("presentation").contentWindow.document;
+
+    iframe.open();
+    iframe.write(publish_presentation(input));
+    iframe.close();
+}
+
+window.uPresent = render_presentation; 
